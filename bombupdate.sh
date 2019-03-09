0381c0e3be85fb821b1acc60d03d70ef30cbfc8d
tuxutku <proje.pdf@outlook.com>
2019-03-09 18:39:44 +0200

    more up-to-date

--- bombupdate.sh
+++ bombupdate.sh
@@ -1,15 +1,19 @@
 #/bin/sh
+echo started
+cd /home/utku/Programlar/Bombsquad/
 w=http://www.files.froemling.net/bombsquad/builds
 a=$(wget -O - $w |grep -o \"BombSquad_Linux_64bit.*.\" |tr -d \" |rev |cut -c 8- |rev)
 l=$(ls)
 if [ $l = $a ]
-echo allready the newest version
-else wget -O - $w/$a.tar.gz |tar -xz
+then echo up-to-date
+else echo downloading
+wget -O - $w/$a.tar.gz |tar -xz
 if [ $? = 0 ]
 then rm -r $l
 chmod -R 777 $a
-echo succes
+echo process successful
 else rm -r $a
-echo failed
+echo process failed
 fi
 fi
+read -n1 -r -p 'Press any key to continue...' key
