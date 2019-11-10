45b96700d45e65af87d6b38e3f299b23fecf25f2
Utku Helvacı <proje.pdf@outlook.com>
2019-11-10 14:23:31 +0300

    fixed problems

--- bombupdate.sh
+++ bombupdate.sh
@@ -4,15 +4,16 @@ cd /home/utku/Programlar/Bombsquad/
 w="https://files.ballistica.net/bombsquad/builds"
 a=$(wget -O - $w |grep -o \>BombSquad_Linux_64bit.*.\</a\>|cut -d\< -f1|cut -c2-)
 l=$(ls)
-if [ $l = $a ]
+b=$(echo $a|cut -d. -f-3)
+if [ $l = $b ]
 then echo up-to-date
 else echo downloading
 wget -O - $(echo $w/$a|tr -d ' ')|tar -xz
 if [ $? = 0 ]
-then rm -r $l
-chmod -R 777 $a
+then rm -rf $l
+chmod -R 777 $b
 echo process successful
-else rm -r $a
+else rm -rf $b
 echo process failed
 fi
 fi
